42ea4ee3c0b593258920c08fc60cc2a2143bda50
Stephen Murray <smurray@nearbycomputing.com>
2021-12-02 18:58:55 +0000

    day 2

--- day02/day02.go
+++ day02/day02.go
@@ -1,1 +1,78 @@
 package day02
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Heading struct {
+	Height int
+	Length int
+	Aim    int
+}
+
+type Move struct {
+	Dir string
+	Val int
+}
+
+func ReadFile() []Move {
+	b, err := os.ReadFile("/home/smurray/adventofcode2021/day02/input")
+	if err != nil {
+		panic(err)
+	}
+	ss := strings.Split(string(b), "\n")
+
+	var mm []Move
+	for _, s := range ss[:len(ss)-1] { // ignore empty string at end
+
+		line := strings.Split(s, " ")
+		i, err := strconv.Atoi(line[1])
+		if err != nil {
+			panic(err)
+		}
+		m := Move{
+			Dir: line[0],
+			Val: i,
+		}
+		mm = append(mm, m)
+		// fmt.Println(m)
+	}
+	return mm
+}
+
+func PartOne(mm []Move) {
+	var h Heading
+
+	for _, m := range mm {
+		switch m.Dir {
+		case "forward":
+			h.Length += m.Val
+		case "down":
+			h.Height += m.Val
+		case "up":
+			h.Height -= m.Val
+		}
+	}
+	fmt.Println(h.Height * h.Length)
+}
+
+func PartTwo(mm []Move) {
+	var h Heading
+
+	for _, m := range mm {
+		switch m.Dir {
+		case "forward":
+			h.Length += m.Val
+			h.Height += (m.Val * h.Aim)
+		case "down":
+			h.Aim += m.Val
+		case "up":
+			h.Aim -= m.Val
+		}
+	}
+
+	fmt.Println(h.Height * h.Length)
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/stephenpmurray/adventofcode2021/day01"
+	"github.com/stephenpmurray/adventofcode2021/day02"
 )
 
 func main() {
@@ -12,5 +13,7 @@ func main() {
 	day01.PartTwo(vals)
 
 	// day 2
-
+	m := day02.ReadFile()
+	day02.PartOne(m)
+	day02.PartTwo(m)
 }
